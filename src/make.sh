08b1fbe4327939922273b1e4c0619e537ab63030
Pernod70 <ngbarnes@hotmail.com>
2014-03-31 01:22:31 +0100

    Added PACKAGE_ICON_120.PNG

--- src/make.sh
+++ src/make.sh
@@ -4,8 +4,8 @@ chown -R root:root desktop etc mediatomb ui
 chmod -R 755 *
 tar -czf package.tgz -X exclude.txt desktop etc mediatomb ui
 
-chown -R root:root scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG
+chown -R root:root scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 chmod -R 755 *
-tar -cvf ../MediaTomb-0.11.0-001.spk -X exclude.txt scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG
+tar -cvf ../MediaTomb-0.11.0-002.spk -X exclude.txt scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 
 exit 0
